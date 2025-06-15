9259d44aba3b6dbc1824eb87e0db9d64b5ab9dd4
TDex <technodexterous.team@gmail.com>
2025-06-16 00:54:02 +0530

    added templates folder

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return "<h1> Hello from Flask Coolify! </h1>"
-
+def home():
+    return render_template('index.html')
 
 @app.route('/bye')
 def bye():
     return "<h1> Bye from Flask Coolify! </h1>"
+
+if __name__ == '__main__':
+    app.run(debug=True)
