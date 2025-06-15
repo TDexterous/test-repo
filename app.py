b7cdd369bc0bbd9c28d389eb38cfadfe42a6adf6
TDex <technodexterous.team@gmail.com>
2025-06-16 01:12:21 +0530

    updated template routes

--- app.py
+++ app.py
@@ -6,9 +6,21 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/onboard')
+def onboard():
+    return render_template('onboarding.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/web-isell')
+def isell():
+    return render_template('isell.html')
+
 @app.route('/bye')
 def bye():
-    return "<h1> Bye from Flask Coolify! </h1>"
+    return "<h1> Bye from WebiSell! </h1>"
 
 if __name__ == '__main__':
     app.run(debug=True)
